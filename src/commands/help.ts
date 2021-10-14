073e79895b7cf1300ba4e562bb97b401e7e13c2f
kevlu8 <kevie.lu8@gmail.com>
2021-10-13 20:17:34 -0600

    Added help command

--- src/commands/help.ts
+++ src/commands/help.ts
@@ -1,9 +1,23 @@
 import { CommandInteraction } from 'discord.js';
 
+const help = new MessageEmbed()
+	.setColor('#0099ff')
+	.setTitle('Help')
+	.setURL('https://www.youtube.com/watch?v=dQw4w9WgXcQ')
+	.addFields(
+		{ name: 'Help', value: 'Displays this message' },
+		{ name: 'Ping', value: 'Checks ping with Discord servers'}
+		//{ name: '', value: ''},
+	)
+	.setTimestamp()
+	.setFooter('Made by kevlu8 and Megatank58', 'https://i.imgur.com/AfFp7pu.png');
+
 export default {
 	name: 'help',
 	description: 'Get a list of all commands',
+    
 	execute(interaction: CommandInteraction) {
-		await interaction.reply('WIP');
+		await interaction.reply({embed: [help] });
 	},
 };
+
--- src/commands/ping.ts
+++ src/commands/ping.ts
@@ -4,6 +4,6 @@ export default {
 	name: 'ping',
 	description: 'Check the ping of the bot',
 	execute(interaction: CommandInteraction) {
-		await interaction.editReply('Pong!');
+		interaction.editReply('Pong!');
 	},
 };
