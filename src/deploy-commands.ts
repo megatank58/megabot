fd1b2c971053804b1832d4c7b93b9fc0ea08afdb
Megatank58 <tanumegatank@gmail.com>
2021-10-16 06:17:58 +0000

    feat: global and guild commands

--- src/deploy-commands.ts
+++ src/deploy-commands.ts
@@ -5,7 +5,8 @@ import { readdirSync } from 'fs';
 
 config();
 
-const commands = [];
+const guildCommands = [];
+const globalCommands = [];
 const commandFiles = readdirSync('.build/commands');
 
 const rest = new REST({ version: '9' }).setToken(process.env.DISCORD_TOKEN!);
@@ -13,10 +14,11 @@ const rest = new REST({ version: '9' }).setToken(process.env.DISCORD_TOKEN!);
 (async () => {
 	for (const file of commandFiles) {
 		const command = await import(`../.build/commands/${file}`);
-		commands.push({ name: command.default.name, description: command.default.description, options: command.default.options, default_permission: command.default.default_permission });
+		const commandData = { name: command.default.name, description: command.default.description, options: command.default.options, default_permission: command.default.default_permission };
+		command.default.isGlobal ? globalCommands.push(commandData) : guildCommands.push(commandData);
 	}
 
-	if (process.env.GUILD_ID) return rest.put(Routes.applicationGuildCommands(process.env.CLIENT_ID!, process.env.GUILD_ID), { body: commands });
+	if (process.env.GUILD_ID) rest.put(Routes.applicationGuildCommands(process.env.CLIENT_ID!, process.env.GUILD_ID), { body: guildCommands });
 
-	rest.put(Routes.applicationCommands(process.env.CLIENT_ID!), { body: commands });
+	rest.put(Routes.applicationCommands(process.env.CLIENT_ID!), { body: globalCommands });
 })();
